99e97f3176828d366c810f9574a4f7e75d95cd82
Aaron Krämer <aaron@cs.rwth-aachen.de>
2017-10-01 21:13:13 +0200

    updated documentation of PASTAView

--- PASTA/Classes/PASTAView.swift
+++ PASTA/Classes/PASTAView.swift
@@ -8,30 +8,31 @@
 
 import UIKit
 
-/**
- `PASTAView` represents the root of the SDK.
- Set yourself as `delegate` to receive status and event updates of every Tangible detected on this view.
-
- This class overrides the `hitTest(_:with:)` function.
- It initializes a `PASTAMarker` if none of the subviews contains the point send with `hitTest(_:with:)`,
- adds it as subview, and returns the marker as a result of `hitTest(_:with:)`.
-
- To change the used `TangibleManager` set the `tangibleManager` property of `markerManager`.
- Don't forget to set `tangibleDelegate` of your new `TangibleManager`.
- Otherwise you won't receive Tangible events.
- */
+/// `PASTAView` represents the base of the SDK.
+/// It creates new `PASTAMarker` by overriding the `hitTest(_:with:)` of `UIView` and
+/// informs the delegate about Tangible status and event changes.
 public class PASTAView: UIView {
 
+    /// Sets the `tangibleDelegate` of `manager`.
+    /// The object will receive status and event updates of every Tangible detected on this view.
     public weak var delegate: TangibleEvent? {
         didSet {
             manager.tangibleDelegate = delegate
         }
     }
     /// The manager used by this view.
+    /// If you assign a new manager do not forget to assign an object to `delegate`.
+    /// Otherwise you will not receive updates.
     public var manager: TangibleManager = PASTAManager()
 
     // hitTest() gets called 2 times
     // http://stackoverflow.com/questions/4304113/uiviews-hittestwithevent-called-three-times#4536765
+
+    /// Initializes a `PASTAMarker` if none of the subviews contains `point`,
+    /// adds the marker as subview, and returns the marker.
+    /// Will never return `self`.
+    /// Returns `nil` if `point` is outside the view.
+    /// - returns: `nil` if `point` outside, a subview if one is hit, otherwise a new `PASTAMarker`.
     public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard self.point(inside: point, with: event) else { return nil }
         if let resultView = super.hitTest(point, with: event), resultView != self {
