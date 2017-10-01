30e5748125782489020917ebb3528ba2381db13f
Aaron Krämer <aaron@cs.rwth-aachen.de>
2017-10-01 23:02:44 +0200

    refactored and fixed PASTATangibleTests

--- Tests/PASTATangibleTests.swift
+++ Tests/PASTATangibleTests.swift
@@ -13,46 +13,37 @@ class PASTATangibleTests: QuickSpec {
         var m1: PASTAMarker!
         var m2: PASTAMarker!
         var m3: PASTAMarker!
-        var t: PASTATangible?
+        var tangible: PASTATangible!
 
         beforeEach {
             m1 = PASTAMarker(center: CGPoint(x: 0, y: 10), radius: 5)
             m2 = PASTAMarker(center: CGPoint.zero, radius: 5)
             m3 = PASTAMarker(center: CGPoint(x: 10, y: 0), radius: 5)
-            t = PASTATangible(markers: [m1, m2, m3])
+            tangible = PASTATangible(markers: [m1, m2, m3])
         }
         describe("a Tangible") {
-            it("is not nil") {
-                expect(t).toNot(beNil())
-            }
             it("is active") {
-                guard let tangible = t else { return }
                 expect(tangible.isActive).to(beFalse())
             }
             it("has center") {
-                guard let tangible = t else { return }
                 expect(tangible.center).to(equal(CGPoint(x: 5, y: 5)))
             }
             it("has markers") {
-                guard let tangible = t else { return }
                 expect(tangible.markers.count).to(equal(3))
             }
             it("has radius") {
-                guard let tangible = t else { return }
                 expect(tangible.radius).to(beCloseTo(14.1421/2.0))
             }
             it("has orientation") {
-                guard let tangible = t else { return }
-                let angle = CGVector.normalizedUp.angle(between: tangible.initialOrientationVector)
+                let angle = CGVector(angle: Angle(.pi/2 * -1), magnitude: 1)
+                        .angle(between: tangible.initialOrientationVector)
                 expect(angle.degrees).to(equal(0))
                 expect(tangible.orientationVector).toNot(beNil())
             }
             it("is similar to itself") {
-                guard let tangible = t else { return }
                 expect(tangible ~ tangible).to(beTrue())
             }
             it("has interior angle") {
-                guard let tangible = t else { return }
                 let m1UuidString = m1.markerSnapshot.uuidString
                 let m2UuidString = m2.markerSnapshot.uuidString
                 let m3UuidString = m3.markerSnapshot.uuidString
@@ -68,13 +59,12 @@ class PASTATangibleTests: QuickSpec {
 
             context("rotated") {
                 it("has orientation") {
-                    guard let tangible = t else { return }
                     let vectorBeforeRotation = tangible.orientationVector
                     let angle = Angle(56, unit: .degrees)
                     m1.center = LineSegment(a: tangible.center, b: m1.center).rotatedAroundA(angle).b
                     m2.center = LineSegment(a: tangible.center, b: m2.center).rotatedAroundA(angle).b
                     m3.center = LineSegment(a: tangible.center, b: m3.center).rotatedAroundA(angle).b
-                    let up = CGVector.normalizedUp
+                    let up = CGVector(angle: Angle(.pi/2 * -1), magnitude: 1)
                     let angle1 = up.angle(between: tangible.initialOrientationVector)
                     expect(angle1.degrees).to(beCloseTo(56))
 
@@ -83,25 +73,24 @@ class PASTATangibleTests: QuickSpec {
                         fail()
                         return
                     }
-                    let angle2 = v.angle(between: v2, absolute: true)
+                    var angle2 = v.angle(between: v2)
+                    angle2 = angle2 < Angle(0) ? angle2.inversed : angle2
                     expect(angle2.degrees).to(beCloseTo(56))
                 }
             }
 
             context("one is moving") {
-                var beforeRadius: CGFloat?
+                var beforeRadius: CGFloat!
                 beforeEach {
-                    beforeRadius = t?.radius
+                    beforeRadius = tangible.radius
                     m1.center = m1.center.applying(.init(translationX: 0, y: -5))
                     m1.tangible?.markerMoved(m1)
                 }
                 it("center changes") {
-                    guard let tangible = t else { return }
                     expect(tangible.center.y) < tangible.previousCenter.y
                 }
                 it("radius changes") {
-                    guard let tangible = t, let befRadius = beforeRadius else { return }
-                    expect(tangible.radius) < befRadius
+                    expect(tangible.radius) < beforeRadius
                 }
             }
 
@@ -110,11 +99,9 @@ class PASTATangibleTests: QuickSpec {
                     m2.isActive = true
                 }
                 it("has two inactive") {
-                    guard let tangible = t else { return }
                     expect(tangible.inactiveMarkers.count) == 2
                 }
                 it("will replace an inactive marker") {
-                    guard let tangible = t else { return }
                     let marker = PASTAMarker(center: CGPoint(x: 2, y: 9), radius: 5)
                     marker.isActive = true
                     let success = tangible.replaceInactiveMarker(with: marker)
@@ -123,7 +110,6 @@ class PASTATangibleTests: QuickSpec {
                     expect(tangible.inactiveMarkers.count) == 1
                 }
                 it("won't replace inactive marker") {
-                    guard let tangible = t else { return }
                     let marker = PASTAMarker(center: CGPoint(x: 4, y: 2.5), radius: 5)
                     marker.isActive = true
                     let success = tangible.replaceInactiveMarker(with: marker)
@@ -137,10 +123,9 @@ class PASTATangibleTests: QuickSpec {
                     beforeEach {
                         m1.isActive = true
                         m3.isActive = true
-                        activePattern = t!.pattern
+                        activePattern = tangible.pattern
                     }
                     it("has no inactive marker") {
-                        guard let tangible = t else { return }
                         expect(tangible.inactiveMarkers.isEmpty) == true
                     }
                     context("m3 is inactive") {
@@ -149,14 +134,12 @@ class PASTATangibleTests: QuickSpec {
                             var touches = Set<MockTouch>()
                             touches.insert(MockTouch(location: m3.center, radius: m3.radius))
                             m3.touchesEnded(touches, with: nil)
-                            inactivePattern = t!.pattern
+                            inactivePattern = tangible.pattern
                         }
                         it("has an inactive marker") {
-                            guard let tangible = t else { return }
                             expect(tangible.inactiveMarkers.count) == 1
                         }
                         it("current and previous pattern are similar") {
-                            guard let tangible = t else { return }
                             expect(activePattern.isSimilar(to: tangible.pattern)) == true
                         }
                         it("m3 marker did not moved") {
@@ -168,14 +151,12 @@ class PASTATangibleTests: QuickSpec {
                                 var touches = Set<MockTouch>()
                                 touches.insert(MockTouch(location: m3.center, radius: m3.radius))
                                 m3.touchesBegan(touches, with: nil)
-                                secondActivePattern = t!.pattern
+                                secondActivePattern = tangible.pattern
                             }
                             it("has no inactive markers") {
-                                guard let tangible = t else { return }
                                 expect(tangible.inactiveMarkers.isEmpty) == true
                             }
                             it("pattern similar to previous") {
-                                guard let tangible = t else { return }
                                 expect(tangible.pattern.isSimilar(to: inactivePattern)) == true
                             }
                             it("m3 marker did not moved") {
@@ -188,14 +169,12 @@ class PASTATangibleTests: QuickSpec {
                                     m3.touchesEnded(touches, with: nil)
                                 }
                                 it("has an inactive marker") {
-                                    guard let tangible = t else { return }
                                     expect(tangible.inactiveMarkers.count) == 1
                                 }
                                 it("m3 marker did not moved") {
                                     expect(m3.center) == m3.previousCenter
                                 }
                                 it("pattern similar to previous") {
-                                    guard let tangible = t else { return }
                                     expect(tangible.pattern.isSimilar(to: secondActivePattern)) == true
                                 }
 
@@ -207,9 +186,9 @@ class PASTATangibleTests: QuickSpec {
 
             context("received a marker-moved event") {
                 var translation: CGPoint!
-                var prevTangibleRadius: CGFloat?
+                var prevTangibleRadius: CGFloat!
                 beforeEach {
-                    prevTangibleRadius = t?.radius
+                    prevTangibleRadius = tangible.radius
                     var touches = Set<MockTouch>()
                     let angle = Angle(45, unit: .degrees)
                     let newMarkerPosition = LineSegment(a: m2.center, b: m1.center).rotatedAroundA(angle).b
@@ -218,11 +197,9 @@ class PASTATangibleTests: QuickSpec {
                     m1.touchesBegan(touches, with: nil)
                 }
                 it("has one active marker") {
-                    guard let tangible = t else { return }
                     expect(tangible.inactiveMarkers.count).to(equal(2))
                 }
                 it("is active") {
-                    guard let tangible = t else { return }
                     expect(tangible.isActive).to(beTrue())
                 }
                 it("active marker moved") {
@@ -236,25 +213,22 @@ class PASTATangibleTests: QuickSpec {
                     expect(m3.center.y).to(beCloseTo(m3.previousCenter.y + translation.y))
                 }
                 it("center moved") {
-                    guard let tangible = t else { return }
                     expect(tangible.center.x).to(beCloseTo(tangible.previousCenter.x + translation.x))
                     expect(tangible.center.y).to(beCloseTo(tangible.previousCenter.y + translation.y))
                 }
                 it("frame updates") {
-                    guard let tangible = t else { return }
                     expect(tangible.frame.origin).to(equal(tangible.center.applying(
                             .init(translationX: -tangible.radius, y: -tangible.radius))))
                     expect(tangible.frame.width).to(equal(tangible.radius*2))
                 }
                 it("radius did not changed") {
-                    guard let tangible = t, let prevRadius = prevTangibleRadius else { return }
-                    expect(tangible.radius).to(beCloseTo(prevRadius))
+                    expect(tangible.radius).to(beCloseTo(prevTangibleRadius))
                 }
 
                 context("received marker-moved event from different marker") {
-                    var prevTangibleRadius2: CGFloat?
+                    var prevTangibleRadius2: CGFloat!
                     beforeEach {
-                        prevTangibleRadius2 = t?.radius
+                        prevTangibleRadius2 = tangible.radius
 
                         var touches2 = Set<MockTouch>()
                         let angle = Angle(45, unit: .degrees)
@@ -263,19 +237,15 @@ class PASTATangibleTests: QuickSpec {
                         m2.touchesBegan(touches2, with: nil)
                     }
                     it("has two active markers") {
-                        guard let tangible = t else { return }
                         expect(tangible.inactiveMarkers.count).to(equal(1))
                     }
                     it("radius did not changed") {
-                        guard let tangible = t, let rad = prevTangibleRadius2 else { return }
-                        expect(tangible.radius).to(beCloseTo(rad))
+                        expect(tangible.radius).to(beCloseTo(prevTangibleRadius2))
                     }
                     it("is active") {
-                        guard let tangible = t else { return }
                         expect(tangible.isActive).to(beTrue())
                     }
                     it("center moved") {
-                        guard let tangible = t else { return }
                         let prevCenter = Triangle(a: m1.center,
                                 b: m2.previousCenter,
                                 c: tangible.inactiveMarkers.first!.previousCenter).cicrumcenter
@@ -286,7 +256,8 @@ class PASTATangibleTests: QuickSpec {
                     }
                     xit("inactive marker moved") {  // FIXME: update
                         let v1 = CGVector(from: m1.center, to: m3.previousCenter)
-                        let angle = CGVector(from: m1.center, to: m3.center).angle(between: v1, absolute: true)
+                        var angle = CGVector(from: m1.center, to: m3.center).angle(between: v1)
+                        angle = angle.degrees < 0 ? angle.inversed : angle
                         expect(angle.degrees).to(beCloseTo(45))
 
                         let rotationAngle = Angle(45, unit: .degrees)
@@ -301,7 +272,8 @@ class PASTATangibleTests: QuickSpec {
                     it("active marker moved") {
                         let cv = CGVector(from: m1.center, to: m2.center)
                         let ov = CGVector(from: m1.center, to: m2.previousCenter)
-                        let angle = cv.angle(between: ov, absolute: true)
+                        var angle = cv.angle(between: ov)
+                        angle = angle < Angle(0) ? angle.inversed : angle
                         expect(angle.degrees).to(beCloseTo(45))
 
                         let rotationAngle = Angle(45, unit: .degrees)
