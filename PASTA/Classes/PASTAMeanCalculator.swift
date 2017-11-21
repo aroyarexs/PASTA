b059d9000af2ec16df1147edfab2a7031c53e357
Aaron Krämer <aaron@cs.rwth-aachen.de>
2017-11-21 09:05:46 +0100

    added new class to determine most frequent value

--- PASTA/Classes/PASTAMeanCalculator.swift
+++ PASTA/Classes/PASTAMeanCalculator.swift
@@ -23,3 +23,39 @@ class PASTAMeanCalculator { // TODO: rename to MeanCalculator
         return mean
     }
 }
+
+/// Counts the frequency of each value which is added.
+class Counter {
+
+    /// A dictionary storing the frequencies for each added value.
+    private var frequencies = [CGFloat: Int]()
+    /// The value which occurs most frequent.
+    private (set) var mostFrequent: CGFloat
+    /// The frequency of the `mostFrequent` value.
+    private var frequency: Int {
+        return frequencies[mostFrequent]!
+    }
+
+    /// Initializes the counter with a value.
+    /// - parameter value: A `CGFloat`.
+    init(value: CGFloat) {
+        mostFrequent = value
+        _ = add(value: value)
+    }
+
+    /// If `value` not present in dictionary it is added with frequency 1, otherwise count is increased by 1.
+    /// - parameter value: A `CGFloat`.
+    /// - returns: The most frequent value.
+    func add(value: CGFloat) -> CGFloat {
+        if let count = frequencies[value] {
+            frequencies[value] = count + 1
+        } else {
+            frequencies[value] = 1
+        }
+
+        if frequency < frequencies[value]! {
+            mostFrequent = value
+        }
+        return mostFrequent
+    }
+}
